237224a9a1cb09517caaa0adbb0205ef67bf8633
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2026-03-11 22:25:24 +0100

    Set up an initial custom theme. Defined a custom logo. Assigned a preview image. Activated the custom theme. 	new file:   Magento_Theme/layout/default.xml 	new file:   composer.json 	new file:   registration.php 	new file:   theme.xml 	new file:   web/images/logo.svg

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Denal05/MacademyM2PowerThemingJuno',
+    __DIR__
+);
